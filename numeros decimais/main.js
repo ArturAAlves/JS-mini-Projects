2d25fcf1587e1363aac6f160c6e05c69ea3f5e9d
Artur Alves <artudoalves@gmail.com>
2020-09-06 15:48:01 +0100

    s
    s

--- numeros decimais/main.js
+++ numeros decimais/main.js
@@ -2,18 +2,21 @@ const input = document.querySelector(".input");
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", () => {
-  let number = (Math.random() * 10).toFixed(1);
-  input.innerText = number;
-  let store = (number % 1).toFixed(1);
-  let endNumber = number - store;
+  let number = Number.parseFloat((Math.random() * 10).toFixed(1));
+  let decimialRest = Number.parseFloat((number % 1));
+  console.log(number)
+  number = number - decimialRest;
 
-  if (store <= 3) {
-    store = 0;
-  } else if (store > 3) {
-    store = 5;
-  } else if (store < 7) {
-    store = 1;
+
+  if (decimialRest <= 0.3) {
+    decimialRest = 0
+  } else if (decimialRest >= 0.7) {
+    decimialRest = 1
+  } else {
+    decimialRest = 0.5;
   }
-  endNumber = number + store;
-  console.log(endNumber);
+
+  console.log(decimialRest)
+
+  input.innerText = number + decimialRest;
 });
