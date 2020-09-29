d9af5b9c6028d348d545f4a355f990f6d20d47d3
Artur Alves <artudoalves@gmail.com>
2020-09-29 21:35:47 +0100

    1.1

--- memory game/dist/main.dev.js
+++ memory game/dist/main.dev.js
@@ -31,43 +31,47 @@ var images = [{
 }];
 var cardBack = document.querySelectorAll(".card-back");
 
-var _loop = function _loop(i) {
-  card[i].addEventListener("click", function () {
-    card[i].classList.toggle('is-flipped');
-  });
-};
+function click() {
+  var _loop = function _loop(i) {
+    storeOne = "";
+    storeArray = [];
+    card[i].addEventListener("click", function () {
+      card[i].classList.add('is-flipped');
+      storeOne = cardBack[i].style.backgroundImage;
+      storeArray.push(storeOne);
 
-for (var i = 0; i < card.length; i++) {
-  _loop(i);
+      if (storeArray[0] == storeArray[1]) {
+        console.log("hello");
+        storeArray = [];
+        click();
+      }
+    });
+  };
+
+  for (var i = 0; i < card.length; i++) {
+    var storeOne;
+    var storeArray;
+
+    _loop(i);
+  }
 }
+
+click();
 
 function randomize() {
   var numberStore = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   var novoArray = [];
 
-  for (var _i = numberStore.length; _i >= 0; _i--) {
+  for (var i = numberStore.length; i >= 0; i--) {
     var copyArray = numberStore;
-    console.log(copyArray);
-    var randomNumber = Math.floor(Math.random() * _i);
+    var randomNumber = Math.floor(Math.random() * i);
     novoArray.push(copyArray[randomNumber]);
     copyArray.splice(randomNumber, 1);
   }
 
-  for (var _i2 = 0; _i2 < cardBack.length; _i2++) {
-    cardBack[_i2].style.backgroundImage = "url(\"".concat(images[novoArray[_i2]].img, "\")");
+  for (var _i = 0; _i < cardBack.length; _i++) {
+    cardBack[_i].style.backgroundImage = "url(\"".concat(images[novoArray[_i]].img, "\")");
   }
 }
 
-randomize(); // function randomizeNumbers() {
-//     var numberStore = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-//     var novoArray = []
-//     for (let i = numberStore.length; i >= 1; i--) {
-//         var copyArray = numberStore
-//         var randomNumber = Math.floor(Math.random() * i);
-//         novoArray.push(copyArray[randomNumber])
-//         console.log(copyArray[randomNumber])
-//         copyArray.splice(randomNumber, 1)
-//     }
-//     // console.log (novoArray)
-// }
-// randomizeNumbers()
+randomize();
